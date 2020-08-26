9d806035ce0e2ff24d4e18abf67f6c951896f7d0
AbhishekMarda <abhishek1marda@ucla.edu>
2020-08-26 22:09:47 +0530

    Initial Commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Abhishek Marda on 8/26/20.
+//  Copyright © 2020 Abhishek Marda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
