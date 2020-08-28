d6fcf3e58f325715637ff84dce0050b6a20a19e9
AbhishekMarda <abhishek1marda@ucla.edu>
2020-08-28 23:20:31 +0530

    removed derived data

--- DerivedData/Tetris/Build/Intermediates.noindex/Tetris.build/Debug/Tetris.build/Objects-normal/x86_64/Block.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/Block/Block.cpp \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/Block/Block.h \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/globals.h \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/Grid/Grid.h
--- DerivedData/Tetris/Build/Intermediates.noindex/Tetris.build/Debug/Tetris.build/Objects-normal/x86_64/Engine.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/Engine/Engine.cpp \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/Engine/Engine.h \
-  /Users/abhishekmarda/Documents/Academics/UCLA/Computer\ Science/Personal\ Projects/Tetris/Tetris/globals.h
